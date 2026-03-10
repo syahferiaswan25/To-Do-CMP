1e503bacc123cd7868295e394edb38e5cc6f15b1
syahferiaswan <muhamad.aswan@axa-mandiri.co.id>
2026-03-10 15:57:19 +0700

    feat: add generate color with lightScheme and darkScheme

--- composeApp/src/commonMain/kotlin/com/aswan/todo/App.kt
+++ composeApp/src/commonMain/kotlin/com/aswan/todo/App.kt
@@ -1,14 +1,19 @@
 package com.aswan.todo
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.aswan.todo.navigation.NavGraph
+import com.aswan.todo.util.darkScheme
+import com.aswan.todo.util.lightScheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    val colorScheme = if (isSystemInDarkTheme()) darkScheme else lightScheme
+
+    MaterialTheme(colorScheme = colorScheme) {
         NavGraph()
     }
 }
